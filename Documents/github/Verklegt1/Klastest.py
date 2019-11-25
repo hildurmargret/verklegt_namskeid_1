63f8a7f6884cab523c94b57d5e001fac6c0f050c
Palina <palina17@ru.is>
2019-11-25 23:26:57 +0000

    more tests

---
+++ Documents/github/Verklegt1/Klastest.py
@@ -0,0 +1,25 @@
+
+
+class Klastest:
+    def __init__(self,var1=0,var2=2):
+        self._var1=var1
+        self.var2=var2
+
+    def hildurprint(self):
+        for i in range(5):
+            print(str(i)+": eg er betri en hildur")
+
+    def hello(self,x):
+        for _ in range(x):
+            print("hello world")
+
+    def test(self):
+        self.hildurprint()
+        self.hello(4)
+
+
+
+if __name__ == "__main__":
+    minnklasi=Klastest()
+    minnklasi.hildurprint()
+    minnklasi.test()
---
+++ Documents/github/Verklegt1/testingklastest.py
@@ -0,0 +1,8 @@
+from Klastest import *
+
+i=1
+while i!=0:
+    minnklasi=Klastest()
+    minnklasi.hildurprint()
+    minnklasi.test()
+    i=int(input("Hi?: "))
