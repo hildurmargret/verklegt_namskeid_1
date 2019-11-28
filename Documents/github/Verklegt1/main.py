2f4018627f596b87aeab022e6f9ffaec2d6c708f
Palina <palina17@ru.is>
2019-11-28 19:57:06 +0000

    bla

--- Documents/github/Verklegt1/main.py
+++ Documents/github/Verklegt1/main.py
@@ -1,16 +1,20 @@
-from frontPage import*
-
-prentLina(40)
-prentSpace(2,40)
-prentButton(13,12,"    Create new    ")
-prentButton(13,12,"  Get information ")
-prentButton(13,12,"      Update      ")
-prentSpace(2,40)
-prentWord(7,"Please choose ['C' create, 'G' get, 'U' update]")
-prentSpace(1,40)
-prentLina(40)
+#!/usr/bin/env python3
+from pagePrints import*
 
 
-#from Staff import *
-#staff=Staff()
-#staff.addInfo()
+
+print_=pagePrints(80,25)
+print_.frontPage()
+
+inp=input()
+
+if inp=="c":
+
+    print_.window1();
+
+input=int(input("number: "))
+if input==1:
+
+    from Staff import *
+    staff=Staff()
+    staff.addInfo()
