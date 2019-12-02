cb60606147e9e086836e51939825aebf9a5260c4
Sara Lind <saralindsveins@gmail.com>
2019-12-02 11:09:59 +0000

    airplane

---
+++ Documents/github/Verklegt1/Airplane.py
@@ -0,0 +1,12 @@
+class Airplane.py:
+    def __init__(self):
+        self.Name=""
+        self.airplaneModel=""
+        self.manufacturer=""
+        self.seats=0
+
+    def addInfo(self):
+        self.Name=input("Airplane Name: ")
+        self.airplaneModel=input("Airplane model: ")
+        self.manufacturer=input("Airplane manufacturer: ")
+        self.seats=int(input("Number of passenger seats: "))
