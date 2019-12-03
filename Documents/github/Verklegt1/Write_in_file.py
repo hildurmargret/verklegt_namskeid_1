3266e5aeb01e66e7caddafedf6b970d25c814c7d
Sara Lind <saralindsveins@gmail.com>
2019-12-03 11:06:14 +0000

    open, write, read

---
+++ Documents/github/Verklegt1/Write_in_file.py
@@ -0,0 +1,18 @@
+import csv
+
+class FileWriter:
+    def __init__(self):
+        self.path='STUDENTDATA/' #laga
+
+    def writeFileWithHeader(self, fileName, header, data):
+        path = self.path + fileName
+        file_object = open(path, 'a') #w ef skrifa eða yfirskrifa
+        writer = csv.writer(file_object)
+        writer.writerow(header)
+        writer.writerow(data)
+
+    if __name__== "__main__":
+        file=FileWriter()
+
+
+        
