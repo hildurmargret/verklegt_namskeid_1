5ad569e7fa62e809039dd534857a6ec3e1f2742e
valdis <valdisbaerings@gmail.com>
2019-12-04 17:28:22 +0000

    meira i getStaff5-8

--- Documents/github/Verklegt1/getStaff5_8.py
+++ Documents/github/Verklegt1/getStaff5_8.py
@@ -11,32 +11,34 @@ file2=path+skra2
 
 inp=5
 inpt='son'
-ssn=''
-rank=''
+ssn=[]
+rank=[]
 
 with open(file2,'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
         ssn,rank=leitaStaff(inpt,file2,ssn,rank)
-        if row['ssn']==ssn:
-            lina=row['ssn']+','+row['name']+','+row['role']+','+row['rank']+','+row['licence']
-            print(lina)
-
+        for i in range(len(ssn)):
+            if row['ssn']==ssn[i]:
+                lina=row['ssn']+','+row['name']+','+row['role']+','+row['rank']+','+row['licence']
+                print(lina)
+                break
 
 with open(file1,'r') as csvFile:
     reader=csv.DictReader(csvFile)
-    for row in reader:
-        if rank=='Flight Attendant':
-            if ssn in row['fa1']:
-                print(row['arrivingAt'])
-            if ssn in row['fa2']:
-                print(row['arrivingAt'])
-        if rank=='Flight Service Manager':
-            if ssn in row['fsm']:
-                print(row['arrivingAt'])
-        if rank=='Captain':
-            if ssn in row['captain']:
-                print(row['arrivingAt'])
-        if rank=='Copilot':
-            if ssn in row['copilot']:
-                print(row['arrivingAt'])
+    for i in range(len(ssn)):
+        for row in reader:
+            if rank[i]=='Flight Attendant':
+                if ssn[i] in row['fa1']:
+                    print(row['arrivingAt'])
+                if ssn[i] in row['fa2']:
+                    print(row['arrivingAt'])
+            elif rank[i]=='Flight Service Manager':
+                if ssn[i] in row['fsm']:
+                    print(row['arrivingAt'])
+            elif rank[i]=='Captain':
+                if ssn[i] in row['captain']:
+                    print(row['arrivingAt'])
+            elif rank[i]=='Copilot':
+                if ssn[i] in row['copilot']:
+                    print(row['arrivingAt'])
