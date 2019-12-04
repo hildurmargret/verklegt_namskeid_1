4f8b43cc81e56b693985c3c06f78c016d8f7fa33
Sara Lind <saralindsveins@gmail.com>
2019-12-04 09:43:02 +0000

    Cancel

--- Documents/github/Verklegt1/Destination.py
+++ Documents/github/Verklegt1/Destination.py
@@ -6,15 +6,31 @@ class Destination:
         self.airport=""
         self.contactName=""
         self.contactNumber=0
+        self.cancel=0
 
     def addInfo(self):
         self.country=input("Country: ")
+        if self.country=="CANCEL":
+            self.country=1
+            return 0
         self.distance=input("Distance: ")
+        if self.distance=="CANCEL":
+            self.distance=1
+            return 0
         self.airport=input("Airport: ")
+        if self.airport=="CANCEL":
+            self.airport=1
+            return 0
         self.contactName=input("Contact name: ")
+        if self.contactName=="CANCEL":
+            self.contactName=1
+            return 0
         self.contactNumber=input("Contact number: ")
+        if self.name=="CANCEL":
+            self.cancel=1
+            return 0
 
 
-if __name__ == '__main__':
-    des=Destination()
-    print(des.country)
+#if __name__ == '__main__':
+#    des=Destination()
+#    print(des.country)
--- Documents/github/Verklegt1/Voyage.py
+++ Documents/github/Verklegt1/Voyage.py
@@ -6,10 +6,26 @@ class Voyage:
         self.numberOfPilots=0
         self.numberOfCabin=0
         self.flightDuration=0
+        self.cancel=0
 
     def addInfo(self):
         self.title=input("Voyage title: ")
+        if self.title=="CANCEL":
+            self.cancel=1
+            return 0
         self.airplaneName=input("Airplane Name: ")
+        if self.airplaneName=="CANCEL":
+            self.cancel=1
+            return 0
         self.numberOfPilots=input("Number of pilots needed: ")
+        if self.numberOfPilots=="CANCEL":
+            self.cancel=1
+            return 0
         self.numberOfCabin=input("Number of cabin crew needed: ")
+        if self.numberOfCabin=="CANCEL":
+            self.cancel=1
+            return 0
         self.flightDuration=input("Flight duration: ")
+        if self.flightDuration=="CANCEL":
+            self.cancel=1
+            return 0
