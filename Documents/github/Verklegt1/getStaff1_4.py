7b2812c0ae30496919bda4220764ab808384ab78
hildurmargret <hildurg17@ru.is>
2019-12-06 15:49:33 +0000

    update employee fyrstu drog

--- Documents/github/Verklegt1/getStaff1_4.py
+++ Documents/github/Verklegt1/getStaff1_4.py
@@ -13,35 +13,61 @@ def staffInfo(inp):
 
     ssn=[]
     rank=[]
-    employees = []
+    employees = {'ssn':[], 'name':[], 'role':[], 'rank':[], 'licence':[], 'address':[], 'phonenumber':[]}
 
     with open(file,'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             if inp==1:
-                empl=row['ssn']+','+row['name']+','+row['role']+','+row['rank']+','+row['licence']+','+row['address']+','+row['phonenumber']
-                arr = empl.split(',')
-                employees.append(arr)
-                #print(lines)
+                employees['ssn'].append(row['ssn'])
+                employees['name'].append(row['name'])
+                employees['role'].append(row['role'])
+                employees['rank'].append(row['rank'])
+                employees['licence'].append(row['licence'])
+                employees['address'].append(row['address'])
+                employees['phonenumber'].append(row['phonenumber'])
+
             elif inp==2:
                 if row['role']=='Pilot':
-                    empl=row['ssn']+','+row['name']+','+row['role']+','+row['rank']+','+row['licence']+','+row['address']+','+row['phonenumber']
-                    arr = empl.split(',')
-                    employees.append(arr)
-                    #print(row['ssn'], row['name'], row['role'], row['rank'], row['licence'])
+                    employees['ssn'].append(row['ssn'])
+                    employees['name'].append(row['name'])
+                    employees['role'].append(row['role'])
+                    employees['rank'].append(row['rank'])
+                    employees['licence'].append(row['licence'])
+                    employees['address'].append(row['address'])
+                    employees['phonenumber'].append(row['phonenumber'])
+
             elif inp==3:
                 if row['role']=='Cabincrew':
-                    empl=row['ssn']+','+row['name']+','+row['role']+','+row['rank']+','+row['address']+','+row['phonenumber']
-                    arr = empl.split(',')
-                    employees.append(arr)
-                    #print(row['ssn'], row['name'], row['role'], row['rank'])
+                    employees['ssn'].append(row['ssn'])
+                    employees['name'].append(row['name'])
+                    employees['role'].append(row['role'])
+                    employees['rank'].append(row['rank'])
+                    employees['licence'].append(row['licence'])
+                    employees['address'].append(row['address'])
+                    employees['phonenumber'].append(row['phonenumber'])
+
             elif inp==4:
                 #inpt=input('Please enter the name or SSN of employee')
                 ssn,rank=leitaStaff(inpt,file,ssn,rank)
                 for i in range(len(ssn)):
                     if row['ssn']==ssn[i]:
-                        empl=row['ssn']+','+row['name']+','+row['role']+','+row['rank']+','+row['licence']+','+row['address']+','+row['phonenumber']
-                        arr = empl.split(',')
-                        employees.append(arr)
+                        employees['ssn'].append(row['ssn'])
+                        employees['name'].append(row['name'])
+                        employees['role'].append(row['role'])
+                        employees['rank'].append(row['rank'])
+                        employees['licence'].append(row['licence'])
+                        employees['address'].append(row['address'])
+                        employees['phonenumber'].append(row['phonenumber'])
+
                         break
     return employees
+
+empl = staffInfo(4)
+
+#end=len(empl['name'])
+
+#for i in range(0,end):
+#    for key in empl:
+#        print(empl[key][i])
+#    print
