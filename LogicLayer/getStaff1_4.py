b3c65f79386962b379c5bb27aa4c796c2fccafde
hildurmargret <hildurg17@ru.is>
2019-12-07 13:57:32 +0000

    getStaff1_4

--- LogicLayer/getStaff1_4.py
+++ LogicLayer/getStaff1_4.py
@@ -38,37 +38,24 @@ def staffInfo(inp):
 
             elif inp==2:
                 if row['role']=='Pilot':
-                    employees['ssn'].append(row['ssn'])
-                    employees['name'].append(row['name'])
-                    employees['role'].append(row['role'])
-                    employees['rank'].append(row['rank'])
-                    employees['licence'].append(row['licence'])
-                    employees['address'].append(row['address'])
-                    employees['phonenumber'].append(row['phonenumber'])
+                    empl = createPilot(self,row['name'],row['ssn'],row['address'],row['phonenumber'],'email',row['rank'],row['role'],row['licence'])
+                    employees.append(empl)
 
             elif inp==3:
-                if row['role']=='Cabincrew':
-                    employees['ssn'].append(row['ssn'])
-                    employees['name'].append(row['name'])
-                    employees['role'].append(row['role'])
-                    employees['rank'].append(row['rank'])
-                    employees['licence'].append(row['licence'])
-                    employees['address'].append(row['address'])
-                    employees['phonenumber'].append(row['phonenumber'])
+                 empl = createCabin(self,row['name'],row['ssn'],row['address'],row['phonenumber'],'email',row['rank'],row['role'])
+                 employees.append(empl)
 
             elif inp==4:
                 #inpt=input('Please enter the name or SSN of employee')
                 ssn,rank=leitaStaff(inpt,file,ssn,rank)
                 for i in range(len(ssn)):
                     if row['ssn']==ssn[i]:
-                        employees['ssn'].append(row['ssn'])
-                        employees['name'].append(row['name'])
-                        employees['role'].append(row['role'])
-                        employees['rank'].append(row['rank'])
-                        employees['licence'].append(row['licence'])
-                        employees['address'].append(row['address'])
-                        employees['phonenumber'].append(row['phonenumber'])
-
+                        if row['licence'] == 'N/A':
+                            empl = createCabin(self,row['name'],row['ssn'],row['address'],row['phonenumber'],'email',row['rank'],row['role'])
+                            employees.append(empl)
+                        else:
+                            empl = createPilot(self,row['name'],row['ssn'],row['address'],row['phonenumber'],'email',row['rank'],row['role'],row['licence'])
+                            employees.append(empl)
                         break
     return employees
 
