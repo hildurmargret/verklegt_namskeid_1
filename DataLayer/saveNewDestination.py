9b0c3a4a75261b69ad537a3b91c1f5eb41649384
Sara Lind <saralindsveins@gmail.com>
2019-12-09 13:35:30 +0000

    Lagað

--- DataLayer/saveNewDestination.py
+++ DataLayer/saveNewDestination.py
@@ -1,7 +1,5 @@
 from ModelClasses.Destination import*
 def saveDestinationInFile(newDest):
-    path="/Users/SaraLind/github/verklegt_namskeid_1/DestinationsCopy.csv"
+    path="/Users/SaraLind/github/verklegt_namskeid_1/csvFiles/DestinationsCopy.csv"
     f=open(path, "a")
     f.write(newDest.airport+","+newDest.country+","+newDest.contactName+","+newDest.contactNumber+"\n")
-
-    
