e736d7979d8a7ebd30ae7393f3d64b5b278d8c76
Palina <palina17@ru.is>
2019-12-10 14:39:33 +0000

    sma lagað

--- UILayer/addInp3.py
+++ UILayer/addInp3.py
@@ -3,29 +3,6 @@ from ModelClasses.Airplane import*
 from LogicLayer.Date import*
 class Inp3():
 
-    def addDestinationInp3(self):
-            self.country=input("Country: ")
-            if self.country=="CANCEL":
-                self.country=1
-                return 0
-            self.distance=input("Distance: ")
-            if self.distance=="CANCEL":
-                self.distance=1
-                return 0
-            self.airport=input("Airport: ")
-            if self.airport=="CANCEL":
-                self.airport=1
-                return 0
-            self.contactName=input("Contact name: ")
-            if self.contactName=="CANCEL":
-                self.contactName=1
-                return 0
-            self.contactNumber=input("Contact phone number: ")
-            if self.contactNumber=="CANCEL":
-                self.cancel=1
-                return 0
-            return self.country, self.distance, self.airport, self.contactName, self.contactNumber
-
 
     def addVoyageInp(self):
 
