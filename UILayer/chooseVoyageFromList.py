c30eb92d57c06f74563e034ac740c14e4a06bef4
hildurmargret <hildurg17@ru.is>
2019-12-11 18:49:33 +0000

    prenta voyage rett

--- UILayer/chooseVoyageFromList.py
+++ UILayer/chooseVoyageFromList.py
@@ -1,8 +1,11 @@
-
+from LogicLayer.Date import*
 def chooseVoyage(voyages):
-    for i in range(len(voyages)):
-        print(i+1,end="")
-        print(' Voyage from KEF to ' + voyages[i].departureFlight.arrivingAt + ", Date:  " + voyages[i].departureFlight.departure)
+    for j in range(len(voyages)):
+        deptTime = str(getDay(voyages[j].departureFlight.departure)) + '/' + str(getMonth(voyages[j].departureFlight.departure)) + '/' + str(getYear(voyages[j].departureFlight.departure))
+        arvlTime = str(getDay(voyages[j].departureFlight.arrival)) + '/' + str(getMonth(voyages[j].departureFlight.arrival)) + '/' + str(getYear(voyages[j].departureFlight.arrival))
+
+        print(j+1,end="")
+        print(' Voyage from KEF to ' + voyages[j].departureFlight.arrivingAt + ", Departure date:  " + deptTime)
     inp=int(input("Number: "))
     voyage=voyages[inp-1]
     return voyage
--- UILayer/printVoyageList.py
+++ UILayer/printVoyageList.py
@@ -2,10 +2,15 @@ from LogicLayer.checkIfManned import*
 def printVoyageList(voyage):
 
     for i in voyage:
+        retDeptTime = str(getDay(i.returnFlight.departure)) + '/' + str(getMonth(i.returnFlight.departure)) + '/' + str(getYear(i.returnFlight.departure)) + ' at ' + str(getHour(i.returnFlight.departure)) + ':' + str(getMinute(i.returnFlight.departure))
+        retArvlTime = str(getDay(i.returnFlight.arrival)) + '/' + str(getMonth(i.returnFlight.arrival)) + '/' + str(getYear(i.returnFlight.arrival)) + ' at ' + str(getHour(i.returnFlight.arrival)) + ':' + str(getMinute(i.returnFlight.arrival))
+        depDeptTime = str(getDay(i.departureFlight.departure)) + '/' + str(getMonth(i.departureFlight.departure)) + '/' + str(getYear(i.departureFlight.departure)) + ' at ' + str(getHour(i.departureFlight.departure)) + ':' + str(getMinute(i.departureFlight.departure))
+        depArvlTime = str(getDay(i.departureFlight.arrival)) + '/' + str(getMonth(i.departureFlight.arrival)) + '/' + str(getYear(i.departureFlight.arrival)) + ' at ' + str(getHour(i.departureFlight.arrival)) + ':' + str(getMinute(i.departureFlight.arrival))
+
         print('VOYAGE DEPARTURE:')
-        print(i.departureFlight.flightNumber + ', ' + i.departureFlight.departingFrom + ' to ' + i.departureFlight.arrivingAt + ', ' + ' Departure: ' + i.departureFlight.departure + ', Arrival: ' + i.departureFlight.arrival)
+        print(i.departureFlight.flightNumber + ', ' + i.departureFlight.departingFrom + ' to ' + i.departureFlight.arrivingAt + ', ' + ' Departure: ' + depDeptTime + ', Arrival: ' + depArvlTime)
         print('VOYAGE RETURN:')
-        print(i.returnFlight.flightNumber + ', ' + i.returnFlight.departingFrom + ' to ' + i.returnFlight.arrivingAt + ', ' + ' Departure: ' + i.returnFlight.departure + ', Arrival: ' + i.returnFlight.arrival)
+        print(i.returnFlight.flightNumber + ', ' + i.returnFlight.departingFrom + ' to ' + i.returnFlight.arrivingAt + ', ' + ' Departure: ' + retDeptTime + ', Arrival: ' + retArvlTime)
 
         cap, cop, fsm = checkIfManned(i.departureFlight)
         if cap and cop and fsm:
