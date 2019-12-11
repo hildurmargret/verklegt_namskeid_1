e243dfaf6af506674773ad48c86d8fb1a3dfa972
hildurmargret <hildurg17@ru.is>
2019-12-11 16:12:31 +0000

    LOGUD FLUGNUMER!!!!!

--- DataLayer/saveUpdatedFlights.py
+++ DataLayer/saveUpdatedFlights.py
@@ -2,12 +2,12 @@ import csv
 def updateFlights(flights):
 
     staff=[]
-    path="/Users/hildur/Documents/github/verklegt_namskeid_1/csvFiles/PastFlights copy.csv"
+    path="/Users/hildur/Documents/github/verklegt_namskeid_1/csvFiles/UpcomingFlights copy3.csv"
 
     f=open(path, 'w')
 
     writer = csv.writer(f)
     writer.writerow( ('flightNumber', 'departingFrom', 'arrivingAt','departure','arrival','aircraftID','captain','copilot','fsm','fa1','fa2') )
-
+    print(len(flights))
     for i in range(len(flights)):
-        writer.writerow((flights[i].flightNumber,flights[i].departingFrom,flights[i].arrivingAt,flights[i].departure,flights[i].arrival,flights[i].aircraftID,flights[i].captain,flights[i].copilot,flights[i].fsm,flights[i].fa1,flights[i].fa2))
+        writer.writerow((flights[i].flightNumber,flights[i].departingFrom,flights[i].arrivingAt,flights[i].departure,flights[i].arrival,flights[i].aircraftId,flights[i].captain,flights[i].copilot,flights[i].fsm,flights[i].fa1,flights[i].fa2))
--- LogicLayer/fixFlightNumbers.py
+++ LogicLayer/fixFlightNumbers.py
@@ -1,44 +1,68 @@
 from DataLayer.getDestinations import*
+from ModelClasses.flightRoute import*
 import operator
 
 
 def fixFlNo(flights, destinations):
 
-    for dest in destinations:
-        if dest.id == 'KEF':
-            del dest
+    for j in range(len(destinations)):
+        if destinations[j].id == 'KEF':
+            del destinations[j]
+
+
+    #print('helo' + str(len(flights)))
+    #print('belo' + str(len(destinations)))
 
     retFlights = []
+    for flight in flights:
+        new_flNo = 'NA'
 
-    for i in range(len(destinations)):
-        flightsToDest_i = []
-        flightsFromDest_i = []
-
-        for flight in flights:
-            new_flNo = 'NA'
-
+        for i in range(len(destinations)):
             if flight.departingFrom == destinations[i].id or flight.arrivingAt == destinations[i].id:
-
                 if (i+1) < 10:
                     new_flNo = new_flNo + '0' + str(i+1)
                 else:
                     new_flNo = new_flNo + str(i+1)
 
-                if flight.departingFrom == destinations[i]:
-                    flightsFromDest_i.append(flight)
-                elif flight.arrivingAt == destinations[i]:
-                    flightsToDest_i.append(flight)
+        flight.flightNumber = new_flNo
 
-            flightsFromDest_i.sort(key=operator.attrgetter('departure'))
-            flightsToDest_i.sort(key=operator.attrgetter('departure'))
+    for j in range(len(destinations)):
+        flightsToDest_i = []
+        flightsFromDest_i = []
 
-            for j in range(len(flightsToDest_i)):
-                new_flNo = new_flNo + str(i*2)
+        for flight in flights:
+            if flight.departingFrom == destinations[j].id:
+                flightsFromDest_i.append(flight)
+            elif flight.arrivingAt == destinations[j].id:
+                flightsToDest_i.append(flight)
 
-            for k in range(len(flightsFromDest_i)):
-                new_flNo = new_flNo + str(i*2 + 1)
+        print(len(flightsFromDest_i))
+        print(len(flightsToDest_i))
 
-            flight.flightNumber = new_flNo
-            retFlights.append(flight)
+        flightsFromDest_i.sort(key=lambda flight: flight.departure)
+        flightsToDest_i.sort(key=lambda flight: flight.departure)
+
+        print('helo' + str(len(flightsFromDest_i)))
+
+        counter = -1
+        day = str(flightsFromDest_i[0].departure[0:10]) + '00:00:00'
+
+        for k in range(len(flightsFromDest_i)):
+            print(k)
+            print(flightsFromDest_i[k].flightNumber)
+            date = str(flightsFromDest_i[k].departure[0:10]) + '00:00:00'
+            if day != date:
+                counter = 0
+            else:
+                counter = counter + 1
+
+            flightsFromDest_i[k].flightNumber = flightsFromDest_i[k].flightNumber + str(counter*2 + 1)
+            flightsToDest_i[k].flightNumber = flightsToDest_i[k].flightNumber + str(counter*2)
+            retFlights.append(flightsToDest_i[k])
+            retFlights.append(flightsFromDest_i[k])
+            day = str(flightsFromDest_i[k].departure[0:10]) + '00:00:00'
+
+        #retFlights.sort(key=lambda flight: flight.departure)
+
 
     return retFlights
