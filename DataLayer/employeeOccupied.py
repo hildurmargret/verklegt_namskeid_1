0e463b083d2d736b3c023fea31c985ae7fb3c990
valdis <valdisbaerings@gmail.com>
2019-12-11 17:55:14 +0000

    laga pinu empocc

--- DataLayer/employeeOccupied.py
+++ DataLayer/employeeOccupied.py
@@ -1,19 +1,23 @@
 import csv
 from LogicLayer.Date import*
 from ModelClasses.flightRoute import*
+from DataLayer.OpenFile import *
 
 def employeeOccupied(employee,retRoute):
     date1=getDay(retRoute.departure)
-    path="/Users/palinakroyer/github/verklegt_namskeid_1/csvFiles/UpcomingFlights copy.csv"
-    with open(path,'r') as File1:
-        print("HIII")
+    file='UpcomingFlights copy.csv'
+    file_=openFile(file)
+
+    #path="/Users/palinakroyer/github/verklegt_namskeid_1/csvFiles/UpcomingFlights copy.csv"
+    with file_ as File1:
+        #print("HIII")
         csv_reader = csv.DictReader(File1)
         for row in csv_reader:
             v = createFlightRoute(row['flightNumber'],row['departingFrom'],row['arrivingAt'],row['departure'],row['arrival'],"",row['captain'],row['copilot'],row['fsm'],row['fa1'],row['fa2'])
             date2=getDay(v.departure)
-            print(date1)
-            print(date2)
-            print(employee.SSN)
+            #print(date1)
+            #print(date2)
+            #print(employee.SSN)
             if date2==date1:
                 if employee.SSN==v.captain:
                     return True
