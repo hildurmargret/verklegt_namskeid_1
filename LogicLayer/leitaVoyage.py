a790002a3f2302ac9b43a1fec74748ba00ddb5f4
hildurmargret <hildurg17@ru.is>
2019-12-11 21:04:31 +0000

    voyage input restrictions

--- LogicLayer/leitaVoyage.py
+++ LogicLayer/leitaVoyage.py
@@ -25,28 +25,20 @@ def leitaVoyage(inpt):
 
         if no%2 == 0 and allPastFlights[j].flightNumber == inpt:
             voyage = createVoyage()
-            depDeptTime = str(getDay(allPastFlights[j].departure)) + '/' + str(getMonth(allPastFlights[j].departure)) + '/' + str(getYear(allPastFlights[j].departure)) + ' at ' + str(getHour(allPastFlights[j].departure)) + ':' + str(getMinute(allPastFlights[j].departure))
-            depArvlTime = str(getDay(allPastFlights[j].arrival)) + '/' + str(getMonth(allPastFlights[j].arrival)) + '/' + str(getYear(allPastFlights[j].arrival)) + ' at ' + str(getHour(allPastFlights[j].arrival)) + ':' + str(getMinute(allPastFlights[j].arrival))
-            arrDeptTime = str(getDay(allPastFlights[j+1].departure)) + '/' + str(getMonth(allPastFlights[j+1].departure)) + '/' + str(getYear(allPastFlights[j+1].departure)) + ' at ' + str(getHour(allPastFlights[j+1].departure)) + ':' + str(getMinute(allPastFlights[j+1].departure))
-            arrArvlTime = str(getDay(allPastFlights[j+1].arrival)) + '/' + str(getMonth(allPastFlights[j+1].arrival)) + '/' + str(getYear(allPastFlights[j+1].arrival)) + ' at ' + str(getHour(allPastFlights[j+1].arrival)) + ':' + str(getMinute(allPastFlights[j+1].arrival))
 
-            depFlight = createFlightRoute(allPastFlights[j].flightNumber, allPastFlights[j].departingFrom, allPastFlights[j].arrivingAt, depDeptTime, depArvlTime,allPastFlights[j].aircraftId, allPastFlights[j].captain, allPastFlights[j].copilot, allPastFlights[j].fsm, allPastFlights[j].fa1, allPastFlights[j].fa2)
+            depFlight = createFlightRoute(allPastFlights[j].flightNumber, allPastFlights[j].departingFrom, allPastFlights[j].arrivingAt,allPastFlights[j].departure,allPastFlights[j].arrival,allPastFlights[j].aircraftId, allPastFlights[j].captain, allPastFlights[j].copilot, allPastFlights[j].fsm, allPastFlights[j].fa1, allPastFlights[j].fa2)
             voyage.departureFlight = depFlight
-            retFlight = createFlightRoute(allPastFlights[j+1].flightNumber, allPastFlights[j+1].departingFrom, allPastFlights[j+1].arrivingAt, arrDeptTime, arrArvlTime,allPastFlights[j+1].aircraftId, allPastFlights[j+1].captain, allPastFlights[j+1].copilot, allPastFlights[j+1].fsm, allPastFlights[j+1].fa1, allPastFlights[j+1].fa2)
+            retFlight = createFlightRoute(allPastFlights[j+1].flightNumber, allPastFlights[j+1].departingFrom, allPastFlights[j+1].arrivingAt,allPastFlights[j+1].departure,allPastFlights[j+1].arrival,allPastFlights[j+1].aircraftId, allPastFlights[j+1].captain, allPastFlights[j+1].copilot, allPastFlights[j+1].fsm, allPastFlights[j+1].fa1, allPastFlights[j+1].fa2)
             voyage.returnFlight = retFlight
             voyages.append(voyage)
 
 
         elif no%2 != 0 and allPastFlights[j].flightNumber == inpt:
             voyage = createVoyage()
-            arrDeptTime = str(getDay(allPastFlights[j].departure)) + '/' + str(getMonth(allPastFlights[j].departure)) + '/' + str(getYear(allPastFlights[j].departure)) + ' at ' + str(getHour(allPastFlights[j].departure)) + ':' + str(getMinute(allPastFlights[j].departure))
-            arrArvlTime = str(getDay(allPastFlights[j].arrival)) + '/' + str(getMonth(allPastFlights[j].arrival)) + '/' + str(getYear(allPastFlights[j].arrival)) + ' at ' + str(getHour(allPastFlights[j].arrival)) + ':' + str(getMinute(allPastFlights[j].arrival))
-            depDeptTime = str(getDay(allPastFlights[j-1].departure)) + '/' + str(getMonth(allPastFlights[j-1].departure)) + '/' + str(getYear(allPastFlights[j-1].departure)) + ' at ' + str(getHour(allPastFlights[j-1].departure)) + ':' + str(getMinute(allPastFlights[j-1].departure))
-            depArvlTime = str(getDay(allPastFlights[j-1].arrival)) + '/' + str(getMonth(allPastFlights[j-1].arrival)) + '/' + str(getYear(allPastFlights[j-1].arrival)) + ' at ' + str(getHour(allPastFlights[j-1].arrival)) + ':' + str(getMinute(allPastFlights[j-1].arrival))
 
-            depFlight = createFlightRoute(allPastFlights[j-1].flightNumber, allPastFlights[j-1].departingFrom, allPastFlights[j-1].arrivingAt, depDeptTime, depArvlTime,allPastFlights[j-1].aircraftId, allPastFlights[j-1].captain, allPastFlights[j-1].copilot, allPastFlights[j-1].fsm, allPastFlights[j-1].fa1, allPastFlights[j-1].fa2)
+            depFlight = createFlightRoute(allPastFlights[j-1].flightNumber, allPastFlights[j-1].departingFrom, allPastFlights[j-1].arrivingAt,allPastFlights[j-1].departure,allPastFlights[j-1].arrival,allPastFlights[j-1].aircraftId, allPastFlights[j-1].captain, allPastFlights[j-1].copilot, allPastFlights[j-1].fsm, allPastFlights[j-1].fa1, allPastFlights[j-1].fa2)
             voyage.departureFlight = depFlight
-            retFlight = createFlightRoute(allPastFlights[j].flightNumber, allPastFlights[j].departingFrom, allPastFlights[j].arrivingAt, arrDeptTime, arrArvlTime,allPastFlights[j].aircraftId, allPastFlights[j].captain, allPastFlights[j].copilot, allPastFlights[j].fsm, allPastFlights[j].fa1, allPastFlights[j].fa2)
+            retFlight = createFlightRoute(allPastFlights[j].flightNumber, allPastFlights[j].departingFrom, allPastFlights[j].arrivingAt,allPastFlights[j].departure,allPastFlights[j].arrival,allPastFlights[j].aircraftId, allPastFlights[j].captain, allPastFlights[j].copilot, allPastFlights[j].fsm, allPastFlights[j].fa1, allPastFlights[j].fa2)
             voyage.returnFlight = retFlight
             voyages.append(voyage)
 
--- UILayer/addInp3.py
+++ UILayer/addInp3.py
@@ -1,60 +1,130 @@
 from ModelClasses.Staff import*
 from ModelClasses.Airplane import*
 from LogicLayer.Date import*
+from DataLayer.getDestinations import*
+from DataLayer.getAircraft import*
 class Inp3():
 
+    def addRouteInp(self,voyage):
+        destinations = getDestinations()
+        aircraft = list_all_aircraft()
 
-    def addRouteInp(self,route):
-        self.flightNumber=input("Flight number: ")
-        if self.flightNumber=="CANCEL":
-            self.cancel=1
-            return 0
-        self.departingFrom=input("Departing from: ")
-        if self.departingFrom=="CANCEL":
-            self.cancel=1
-            return 0
-        self.arrivingAt=input("Arriving at: ")
-        if self.arrivingAt=="CANCEL":
-            self.cancel=1
-            return 0
-        #self.departureDate=input("Departure date: ")
-        date=input("Departure date: ")
-        if date=="CANCEL":
-            self.cancel=1
-            return 0
-        month=input("Departure month: ")
-        if month=="CANCEL":
-            self.cancel=1
-            return 0
-        year=input("Departure year: ")
-        if year=="CANCEL":
-            self.cancel=1
-            return 0
-        hour=input("Departure hour: ")
-        if hour=="CANCEL":
-            self.cancel=1
-            return 0
-        minute=input("Departure minute: ")
-        if minute=="CANCEL":
-            self.cancel=1
-            return 0
+        departRoute = voyage
+
+        validDest_bool = 0
+        correctDest_bool=0
+        while not validDest_bool:
+            self.arrivingAt=input("Destination: ")
+            for dest in destinations:
+                if self.arrivingAt == dest.id and dest.id != 'KEF':
+                    correctDest_bool=1
+            if self.arrivingAt=="CANCEL":
+                validDest_bool = 1
+                self.cancel=1
+                return 0
+            elif not correctDest_bool:
+                print('Invalid input. Destination must be one of the following:')
+                for dest in destinations:
+                    if dest.id != 'KEF':
+                        print(dest.id)
+            else:
+                validDest_bool = 1
+
+        validDate_bool = 0
+        while not validDate_bool:
+            date=input("Departure day [dd]: ")
+            if date=="CANCEL":
+                validDate_bool=1
+                self.cancel=1
+                return 0
+            elif not date.isdigit() or (len(date) != 2):
+                print('Invalid input. Departure day must consist of two integers [0-9]')
+            elif int(date)<1 or int(date)>31:
+                print('Invalid input. Departure day must be in the range 1-31')
+            else:
+                validDate_bool=1
+
+        validMonth_bool=0
+        while not validMonth_bool:
+            month=input("Departure month [mm]: ")
+            if month=="CANCEL":
+                validMonth_bool=1
+                self.cancel=1
+                return 0
+            elif not month.isdigit() or (len(month) != 2):
+                print('Invalid input. Departure month must consist of two integers [0-9]')
+            elif int(month)<1 or int(month)>12:
+                print('Invalid input. Departure month must be in the range 1-12')
+            else:
+                validMonth_bool=1
+
+        validYear_bool=0
+        while not validYear_bool:
+            year=input("Departure year [yyyy]: ")
+            if year=="CANCEL":
+                validYear_bool=1
+                self.cancel=1
+                return 0
+            elif not year.isdigit() or (len(year) != 4):
+                print('Invalid input. Departure year must consist of four integers [0-9]')
+            elif int(year)<2000 or int(year)>2050:
+                print('Invalid input. Departure year must be in the range 2000-2050')
+            else:
+                validYear_bool=1
+
+        validHour_bool = 0
+        while not validHour_bool:
+            hour=input("Departure hour [hh]: ")
+            if hour=="CANCEL":
+                validHour_bool = 1
+                self.cancel=1
+                return 0
+            elif not hour.isdigit() or (len(hour) != 2):
+                print('Invalid input. Departure hour must consist of two integers [0-9]')
+            elif int(hour)<0 or int(hour)>23:
+                print('Invalid input. Departure hour must be in the range 0-23')
+            else:
+                validHour_bool=1
+
+        validMin_bool = 0
+        while not validMin_bool:
+            minute=input("Departure minute [mm]: ")
+            if minute=="CANCEL":
+                validMin_bool = 1
+                self.cancel=1
+                return 0
+            elif not minute.isdigit() or (len(minute) != 2):
+                print('Invalid input. Departure minute must consist of two integers [0-9]')
+            elif int(minute)<0 or int(minute)>59:
+                print('Invalid input. Departure minute must be in the range 0-59')
+            else:
+                validMin_bool=1
+
         date=int(date)
         month=int(month)
         year=int(year)
         hour=int(hour)
         minute=int(minute)
+
         self.departure = getDate(year,month,date,hour,minute)
+        self.arrival=add_hour(self.departure,2)
 
-        hour=hour+2
-        if hour >= 24:
-            hour=hour-24
-            date=date+1
-
-        self.arrival=getDate(year,month,date,hour,minute)
-
-        self.aircraftId=input("Aircraft ID: ")
-        if self.aircraftId=="CANCEL":
-            self.cancel=1
-            return 0
+        validAirID_bool = 0
+        correctAirID_bool=0
+        while not validAirID_bool:
+            self.aircraftId=input("Aircraft ID: ")
+            for plane in aircraft:
+                if self.aircraftId == plane.planeInsignia:
+                    correctAirID_bool=1
+            if self.aircraftId=="CANCEL":
+                validAirID_bool=1
+                self.cancel=1
+                return 0
+            elif not correctAirID_bool:
+                print('Invalid input. Aircraft ID must be one of the following:')
+                for plane in aircraft:
+                    print(plane.planeInsignia)
+            else:
+                validAirID_bool = 1
 
         return self
