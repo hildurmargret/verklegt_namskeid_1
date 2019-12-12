22d1d7f16f2523fad09467a28c3f4898d9cd3bde
valdis <valdisbaerings@gmail.com>
2019-12-12 19:00:51 +0000

    ble

--- UILayer/chooseDestinationFromList.py
+++ UILayer/chooseDestinationFromList.py
@@ -1,8 +1,8 @@
 def DestinationFromList(dest):
-    
+
     for i in range(len(dest)):
         print(i+1,end=" ")
-        print(dest[i].id + ", " + dest[i].destination + ", "+ dest[i].country + ", "  + dest[i].distance + ", " + dest[i].contactName + ", " + dest[i].contactNumber)
-    inp=int(input("destination: "))
+        print(dest[i].id + ", " + dest[i].destination) # + ", "+ dest[i].country + ", "  + dest[i].distance + ", " + dest[i].contactName + ", " + dest[i].contactNumber)
+    inp=int(input("Destination: "))
     dest=dest[inp-1]
     return dest
