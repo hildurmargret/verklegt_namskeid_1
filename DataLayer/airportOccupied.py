fc2f8aa62cb0def8588878a6e2bbb444e3c6bc3e
Sara Lind <saralindsveins@gmail.com>
2019-12-13 18:24:53 +0000

    KOMMENT

--- DataLayer/airportOccupied.py
+++ DataLayer/airportOccupied.py
@@ -12,9 +12,9 @@ def airportOccupied(newVoyage):
         csv_reader = csv.DictReader(File1)
         for row in csv_reader:
             v = createFlightRoute(row['flightNumber'],row['departingFrom'],row['arrivingAt'],row['departure'],row['arrival'],row['aircraftID'],row['soldTickets'],row['captain'],row['copilot'],row['fsm'],row['fa1'],row['fa2'])
-            voy.append(v)
+            voy.append(v) #Lesid ur UpcomingFlights i listann voy
             oldDate=v.departure
-
+#Farid i gegnum oll flug i UpcomingFlights og departure timi borinn saman vid departure tima a flugi sem verid er ad baeta inn, ef their eru their somu er boolAirportOccupied breytt i True
             if newDate==oldDate:
                 print("The airport is already occupied")
                 boolAirportOccupied=True
@@ -22,4 +22,5 @@ def airportOccupied(newVoyage):
     if boolAirportOccupied==False:
         voy.append(depFlight)
         voy.append(retFlight)
+        #Nyju flugunum er baett vid ef flugvollurinn er ekki fratekinn
     return voy
--- DataLayer/employeeOccupied.py
+++ DataLayer/employeeOccupied.py
@@ -7,27 +7,29 @@ from DataLayer.read_pastFlights import*
 def employeeOccupied(employee,voyage):
 
     today=now()
-
+    #Ef dagsetning flugferdar er staerri en nuverandi dagsetning er notad PastFlights copy, annars UpcomingFlights copy
     if today>voyage.departureFlight.departure:
         file='PastFlights copy.csv'
     else:
         file='UpcomingFlights copy3.csv'
 
+    #Flug lesin inn ur theirri skra sem er valin
     flights = read_pastFlights(file)
-
+    #Ar,manudur og dagsetningar settar i ser breytur
     voyYear = int(voyage.departureFlight.departure[0:4])
     voyMonth = int(voyage.departureFlight.departure[5:7])
     voyDay = int(voyage.departureFlight.departure[8:10])
-
+    #Fall sem breytir i isoformat
     voyDate = getDate(voyYear,voyMonth,voyDay,0,0)
 
     for flight in flights:
+        #Farid i gegnum oll flug i flights: ar,manudur og dagsetningar settar i ser breytur
         flYear = int(flight.departure[0:4])
         flMonth = int(flight.departure[5:7])
         flDay = int(flight.departure[8:10])
-
+        #Fall sem breytir i isoformat
         flDate = getDate(flYear,flMonth,flDay,0,0)
-
+        #Ef dagsetningarnar og starfsskodad hvort 
         if voyDate==flDate:
             if employee.SSN==flight.captain:
                 return True
