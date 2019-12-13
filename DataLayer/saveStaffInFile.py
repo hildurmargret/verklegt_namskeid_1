2b65b1998f1113df2a5c397a4b37b8a34ee6ef47
hildurmargret <hildurg17@ru.is>
2019-12-13 16:06:20 +0000

    :)

--- DataLayer/saveStaffInFile.py
+++ DataLayer/saveStaffInFile.py
@@ -3,8 +3,3 @@ def saveStaffInFile(newStaff):
     path="/Users/hildur/Documents/github/verklegt_namskeid_1/csvFiles/CrewCopy.csv"
     f=open(path, "a")
     f.write(newStaff.SSN+","+newStaff.name+","+newStaff.role+","+newStaff.rank+","+newStaff.licence+","+newStaff.address+","+newStaff.phoneNumber+"\n")
-
-# def saveCabinInFile(newStaff):
-#     path="/Users/palinakroyer/github/verklegt_namskeid_1/csvFiles/CrewCopy.csv"
-#     f=open(path, "a")
-#     f.write(newStaff.SSN+","+newStaff.name+","+newStaff.role+","+newStaff.rank+","+"N/A"+newStaff.address+","+newStaff.phoneNumber+"\n")
